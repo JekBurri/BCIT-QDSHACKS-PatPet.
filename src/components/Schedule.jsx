bb3c86728c662f5701b790082e49c5d02d902671
JekBurri <onorbtanko@gmail.com>
2024-03-09 00:14:59 -0800

    created schedule component

---
+++ src/components/Schedule.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+export default function Schedule() {
+  const [schedule, setSchedule] = useState({
+    monday: { morning: false, afternoon: false, evening: false },
+    tuesday: { morning: false, afternoon: false, evening: false },
+    wednesday: { morning: false, afternoon: false, evening: false },
+    thursday: { morning: false, afternoon: false, evening: false },
+    friday: { morning: false, afternoon: false, evening: false },
+    saturday: { morning: false, afternoon: false, evening: false },
+    sunday: { morning: false, afternoon: false, evening: false },
+  });
+
+  return (
+    <div className="p-2 flex flex-col gap-4">
+      <p className='text-lg mb-2'>Your Schedule</p>
+      {Object.keys(schedule).map((day) => (
+        <div key={day} className="flex items-center gap-4">
+          <p className="w-20 text-left">{day.charAt(0).toUpperCase() + day.slice(1)}</p>
+          <div className="flex gap-4">
+            {Object.keys(schedule[day]).map((time) => (
+              <p
+                key={time}
+                className={`px-6 rounded-md ${
+                  schedule[day][time]
+                    ? 'bg-[#FFC63A] border border-[#FFC63A]'
+                    : 'bg-white border border-black'
+                }`}
+                onClick={() =>
+                  setSchedule((prevSchedule) => ({
+                    ...prevSchedule,
+                    [day]: { ...prevSchedule[day], [time]: !prevSchedule[day][time] },
+                  }))
+                }
+              >
+                {time.charAt(0).toUpperCase() + time.slice(1)}
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
